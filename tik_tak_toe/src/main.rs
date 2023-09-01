21f12cba542b0aeec63db20d97ee31803a7f42d6
itsfloki <coder.abhijit90@gmail.com>
2023-09-01 12:10:10 +0530

    add: 3*3 grid

--- tik_tak_toe/src/main.rs
+++ tik_tak_toe/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Tik Tak Toe")]
+async fn main() {
+    let grid_thickness = 2.0;
+    let grid_width = 500;
+    let grid_height = 500;
+    let rows = 3;
+    let cols = 3;
+
+    let cell_width = (grid_width / cols) as f32;
+    let cell_height = (grid_height / rows) as f32;
+
+    loop {
+        clear_background(DARKGRAY);
+
+        // draw grid
+        for row in 0..rows {
+            for col in 0..cols {
+                let cell_x = col as f32 * cell_width;
+                let cell_y = row as f32 * cell_height;
+
+                draw_rectangle_lines(
+                    cell_x,
+                    cell_y,
+                    cell_width,
+                    cell_height,
+                    grid_thickness,
+                    WHITE,
+                )
+            }
+        }
+
+        next_frame().await
+    }
 }
