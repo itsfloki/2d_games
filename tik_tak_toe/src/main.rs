18595de92f880aa8b2d85bcd339de11fca1e07fc
itsfloki <coder.abhijit90@gmail.com>
2023-09-02 08:55:06 +0530

    add: cross & circle shape

--- tik_tak_toe/src/main.rs
+++ tik_tak_toe/src/main.rs
@@ -14,6 +14,24 @@ async fn main() {
     loop {
         clear_background(DARKGRAY);
 
+        // draw circle
+        draw_circle_lines(
+            screen_width() - 100.0,
+            screen_height() - 100.0,
+            30.0,
+            10.0,
+            YELLOW,
+        );
+
+        // draw cross
+        draw_cross(
+            screen_width() - 300.0,
+            screen_width() - 100.0,
+            70.0,
+            10.0,
+            YELLOW
+        );
+
         // draw grid
         for row in 0..rows {
             for col in 0..cols {
@@ -33,4 +51,25 @@ async fn main() {
 
         next_frame().await
     }
+}
+
+fn draw_cross(x: f32, y: f32, line_length: f32, thickness: f32, color: Color) {
+    // top-left endpoint
+    let x1 = x - (line_length / 2.0);
+    let y1 = y - (line_length / 2.0);
+
+    // bottom-right endpoint
+    let x2 = x + (line_length / 2.0);
+    let y2 = y + (line_length / 2.0);
+
+    // top_right endpoint
+    let x3 = x + (line_length / 2.0);
+    let y3 = y - (line_length / 2.0);
+
+    // bottom-left endpoint
+    let x4 = x - (line_length / 2.0);
+    let y4 = y + (line_length / 2.0);
+
+    draw_line(x1, y1, x2, y2, thickness, color);
+    draw_line(x3, y3, x4, y4, thickness, color);
 }
